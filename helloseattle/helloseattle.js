331f96a74de07e227270bc89838a67c5531aa8c4
lisnb.pc <lisnb.h@hotmail.com>
2015-06-20 15:22:53 +0800

    init

---
+++ helloseattle/helloseattle.js
@@ -0,0 +1,10 @@
+var leetcode = {
+    leetcodeurl: "https://leetcode.com/problemset/algorithms/",
+    redirect:function(){
+        alert("你女票等着你呢，看你麻痹，刷题去！");
+        alert("快去啊！")
+        window.location.href = leetcode.leetcodeurl;
+    }
+}
+
+leetcode.redirect();
